7c086a31af610a9df49948dfda3be5f46e56bcab
Manuel Chime <manuel.y.chime@gmail.com>
2026-02-26 11:19:03 +0100

    added operate function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let num1;
+let num2;
+let op;
 
 function add(num1,num2){
     return num1 + num2;
@@ -11,4 +13,23 @@ function multiply(num1,num2) {
 }
 function divide(num1,num2) {
     return num1 / num2;
+}
+
+function operate(num1,num2,op) {
+    switch (op) {
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return subtract(num1,num2);
+            break;
+        case '*':
+             return multiply(num1,num2);
+            break;
+        case '/':
+             return divide(num1,num2);
+            break;
+        default:
+            break;
+    }
 }
