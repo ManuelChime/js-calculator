98b7a22e40d71db26dc1c7316cad7fd63cc9e7bf
Manuel Chime <manuel.y.chime@gmail.com>
2026-03-02 10:00:53 +0100

    Added the basic arithmetic functions

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
-let num1;
-let num2;
-let op;
+const display = document.querySelector("#display")
+const buttons = document.querySelectorAll(".btn")
+
+let currentInput = "";
+let prevInput = "";
+let op = null;
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click',function (e) {
+        let val = e.target.value;
+        if (val === "C") {
+            currentInput = "";
+            prevInput = "";
+            op = null;
+            updateDisplay(0);
+            return;
+        }
+        if (val === "D") {
+            currentInput = currentInput.slice(0, -1);
+            updateDisplay(currentInput);
+            return;
+        }
+        if (val === "=") {
+            if (currentInput === "" || prevInput === "" ) return; //check if we have enough data to operate
+            currentInput = operate();
+            updateDisplay(currentInput);
+            prevInput = currentInput;
+            currentInput = "";
+
+            return;
+        }
+
+        if (["+","-","*","/"].includes(val)) { //check if the button is operator
+            handleOperator(val);
+            return;
+        }
+        currentInput += val;
+        updateDisplay(currentInput);
+    })
+}
+);
+
+function handleOperator(nextOp){
+    if (currentInput === "") {
+        op = nextOp; // Just change the operator and wait
+        return;
+    }
+    if (currentInput && op) {
+        currentInput =  operate();
+        updateDisplay(currentInput);
+    }
+    prevInput = currentInput;
+    currentInput = "";
+    op = nextOp;
+}
 
 function add(num1,num2){
     return num1 + num2;
@@ -15,7 +67,9 @@ function divide(num1,num2) {
     return num1 / num2;
 }
 
-function operate(num1,num2,op) {
+function operate() {
+    let num1 = Number(prevInput);
+    let num2 = Number(currentInput);
     switch (op) {
         case '+':
             return add(num1,num2);
@@ -32,4 +86,7 @@ function operate(num1,num2,op) {
         default:
             break;
     }
+}
+function updateDisplay(value){
+    display.textContent = value;
 }
